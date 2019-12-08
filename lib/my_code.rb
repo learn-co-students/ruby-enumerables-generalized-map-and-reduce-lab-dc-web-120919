fcb59557f0cb4fd5a4437388ac9618fc19bc157b
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-08 23:38:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(x)
+  array = []
+  i = 0 
+  while i < x.length 
+    array << (yield(x[i]))
+    i += 1 
+  end 
+  array
+end 
+
+def reduce(array, starting_value = nil)
+  if starting_value
+accum = starting_value
+    i = 0
+  else
+    accum = array[0]
+    i = 1
+  end
+  while i < array.length
+    accum = yield(accum, array[i])
+    i += 1
+  end
+  accum
+end
